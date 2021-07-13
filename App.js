581a8c4f9dca31da64cfd0da0dc3908b01b31879
Gustavo Silva <gustavos_barros@hotmail.com>
2021-07-13 18:54:55 -0300

    input colocado em modal

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet,
-  Text,
   View,
-  TextInput,
   Button,
-  ScrollView,
   FlatList
 } from 'react-native';
 
@@ -14,19 +11,33 @@ import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle =>{
     setCourseGoals(currentGoals =>[
-      ...courseGoals, {key: Math.random().toString(), value: goalTitle }
+      ...courseGoals, {id: Math.random().toString(), value: goalTitle }
     ]);
+    setIsAddMode(false);
+  }
+
+  const removeGoalHandler = goalId =>{
+    setCourseGoals(currentGoals =>{
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    })
+  }
+
+  const cancelAddGoalHandler = () =>{
+    setIsAddMode(false);
   }
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal = {addGoalHandler} />
+      <Button title= "Add New Goal" onPress={() => setIsAddMode(true)}/>
+      <GoalInput visible = {isAddMode} onAddGoal = {addGoalHandler} onCancel={cancelAddGoalHandler} />
       <FlatList
+      keyExtractor ={(item, index)=> item.id}
         data={courseGoals}
-        renderItem={itemData => <GoalItem onDelete={() => console.log("ta funfando?")} title={itemData.item.value}/>}  
+        renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value}/>}  
       />
      
     </View>
